661527935c1fcad7fdf7adf1c2ecc79fd87f1263
Pierre Dugue <pierre.dugue1@gmail.com>
2018-05-27 12:56:06 +1000

    fix merge

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-=======
-$(document).ready(function () {
-    $('#fullpage').fullpage({
-        anchors: ['home', 'about', 'skills', 'book'],
-        menu: '#menu',
-        css3: true,
-        // onLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex) {
-        //     if ($(this).attr("class").includes("homePage1")) {
-        //         $("body").css("background","linear-gradient(270deg, #EB5757, #AD00B0)").fadeIn(2000);
-        //     }
-        //     if ($(this).attr("class").includes("aboutPage1")) {
-        //         $("body").css("background","linear-gradient(150deg, #EB5757, #F2994A)").fadeIn(2000);
-        //     }
-        //     if ($(this).attr("class").includes("skillsPage1")) {
-        //         $("body").css("background","linear-gradient(150deg, #AD00B0, #F2994A)").fadeIn(2000);
-        //     }
-        // }
-    });
-});
->>>>>>> 80906fa11d7ef8b7dc472e212cb21966bdeaa346
 
 $(window).load(function () {
     // Animate loader off screen
